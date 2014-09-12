9cf3cd13430db1fd534a694356155dbef674d4e1
Lucas Allan Amorim <lucas.allan@gmail.com>
2014-09-12 16:08:44 -0700

    Added JUnit

---
+++ src/test/java/translator/TranslatorTest.java
@@ -0,0 +1,20 @@
+package translator;
+
+import org.jlua.main.translator.Translator;
+import org.junit.Test;
+import org.luaj.vm2.ast.Stat;
+
+/**
+ * Created by Lucas Allan Amorim on 2014-09-12.
+ */
+public class TranslatorTest {
+
+    protected Translator translator = new Translator();
+
+    @Test
+    public void testIfThenElse() {
+        //Stat.IfThenElse statement = new Stat.IfThenElse();
+
+        //translator.visitIfThenElse(null);
+    }
+}

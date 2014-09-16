184e3fc7a634002ee35c7e8a5ff14b4035243e74
Chris Seaton <chris@chrisseaton.com>
2014-09-16 20:53:30 +0100

    Specialisations can also be slow path.

--- src/main/java/org/jlua/main/builtins/LuaPrintlnBuiltin.java
+++ src/main/java/org/jlua/main/builtins/LuaPrintlnBuiltin.java
@@ -3,54 +3,37 @@ package org.jlua.main.builtins;
 import com.oracle.truffle.api.CompilerDirectives;
 import com.oracle.truffle.api.dsl.Specialization;
 
-import java.io.PrintStream;
-
 /**
  * Created by Lucas Allan Amorim on 2014-09-14.
  */
 public abstract class LuaPrintlnBuiltin extends LuaBuiltinNode {
 
+    @CompilerDirectives.SlowPath
     @Specialization
     public long println(long value) {
-        doPrint(System.out, value);
+        System.out.println(value);
         return value;
     }
 
     @CompilerDirectives.SlowPath
-    private static void doPrint(PrintStream out, long value) {
-        out.println(value);
-    }
-
     @Specialization
     public boolean println(boolean value) {
-        doPrint(System.out, value);
+        System.out.println(value);
         return value;
     }
 
     @CompilerDirectives.SlowPath
-    private static void doPrint(PrintStream out, boolean value) {
-        out.println(value);
-    }
-
     @Specialization
     public String println(String value) {
-        doPrint(System.out, value);
+        System.out.println(value);
         return value;
     }
 
     @CompilerDirectives.SlowPath
-    private static void doPrint(PrintStream out, String value) {
-        out.println(value);
-    }
-
     @Specialization
     public Object println(Object value) {
-        doPrint(System.out, value);
+        System.out.println(value);
         return value;
     }
 
-    @CompilerDirectives.SlowPath
-    private static void doPrint(PrintStream out, Object value) {
-        out.println(value);
-    }
 }
